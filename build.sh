f0be710414618a6112ff47a93150c05d8955fcbf
jheistand <37549610+jheistand@users.noreply.github.com>
2020-12-10 08:30:55 -0600

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export TF_STATE=./terraform
+cd terraform
+terraform init
+terraform plan --var-file="lab.tfvars"
+# terraform apply --auto-approve --var-file="lab.tfvars"
+terraform apply --var-file="lab.tfvars"
+sleep 60s
+cd ..
+TF_STATE=./terraform/
+# Run the playbook (serial)
+# ansible-playbook --inventory-file=/home/jheistand/tf-inv/terraform-inventory ./ansible/playbook.yml -e @./ansible/vars.yml
+#
+# Run the playbooy (async)
+ansible-playbook --inventory-file=/home/jheistand/tf-inv/terraform-inventory ./ansible/playbook-async.yml -e @./ansible/vars.yml
+# director occasionally fails, moving to separate playbook for now
+ansible-playbook --inventory-file=/home/jheistand/tf-inv/terraform-inventory ./ansible/dir.yml -e @./ansible/vars.yml
