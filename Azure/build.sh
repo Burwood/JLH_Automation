e989e681b55813986256e1be308f08f7eb5b9a7c
James Heistand <37549610+jheistand@users.noreply.github.com>
2021-01-26 15:04:57 -0600

    Update build.sh

--- Azure/build.sh
+++ Azure/build.sh
@@ -3,6 +3,8 @@
 pass testuser > /dev/null
 export TF_VAR_adminuser=$(pass adminuser)
 export TF_VAR_adminpass=$(pass adminpass)
+export TF_VAR_tenant_id=$(pass tenant_id)
+export TF_VAR_subscription_id=$(pass subscription_id)
 terraform init
 terraform plan --var-file="azure.tfvars"
 terraform apply --var-file="azure.tfvars"
